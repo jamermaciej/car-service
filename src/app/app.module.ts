1e87140e4a54625c8717a6c278f01e756c0639ae
Maciej Jamer <mjamer@concisesoftware.com>
2020-04-21 15:45:30 +0200

    remove material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
 import { CoreModule } from './core/core.module';
 
 import { AngularFireModule } from '@angular/fire';
--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -1,8 +1,9 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './components/registration/registration.component';
-import { MaterialModule } from '../material.module';
+
 
 
 
@@ -11,7 +12,7 @@ import { MaterialModule } from '../material.module';
   imports: [
     CommonModule,
     RegistrationRoutingModule,
-    MaterialModule
+    MatFormFieldModule
   ]
 })
 export class RegistrationModule { }
