0d00e77c323e22b18530283bf553e7ddc3a298fb
Maciej Jamer <mjamer@concisesoftware.com>
2020-04-21 14:50:37 +0200

    registration module basic configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/registration',
+    pathMatch: 'full'
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/registration/registration.module.ts
@@ -0,0 +1,15 @@
+import { RegistrationRoutingModule } from './registration-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+
+
+@NgModule({
+  declarations: [RegistrationComponent],
+  imports: [
+    CommonModule,
+    RegistrationRoutingModule
+  ]
+})
+export class RegistrationModule { }
