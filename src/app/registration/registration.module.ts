175a69cec8cc59e7b1cc1c70db4d6860fd962216
Maciej Jamer <mjamer@concisesoftware.com>
2020-04-21 16:22:29 +0200

    add example material module to registraion module

--- src/app/registration/registration.module.ts
+++ src/app/registration/registration.module.ts
@@ -3,16 +3,15 @@ import { RegistrationRoutingModule } from './registration-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './components/registration/registration.component';
-
-
-
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [RegistrationComponent],
   imports: [
     CommonModule,
     RegistrationRoutingModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class RegistrationModule { }
