5c92e49ff71f931cd5b7cd072756b0ae04398492
Maciej Jamer <mjamer@concisesoftware.com>
2020-06-02 13:16:05 +0200

    add dynamic config email domain

--- src/app/registration/components/registration/registration.component.ts
+++ src/app/registration/components/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { EmailDomainValidator } from './../../../shared/validators/email-domain-validator';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { emailDomain } from '../../../../assets/config.json';
 
 @Component({
   selector: 'app-registration',
@@ -15,7 +16,7 @@ export class RegistrationComponent implements OnInit {
   ngOnInit(): void {
     this.registrationForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
-      email: ['', [Validators.required, Validators.email, EmailDomainValidator.validateEmailDomain('gmail.com')]],
+      email: ['', [Validators.required, Validators.email, EmailDomainValidator.validateEmailDomain(emailDomain)]],
       password: ['', [Validators.required]],
       password2: ['', [Validators.required]],
       terms: ['', [Validators.required]]
