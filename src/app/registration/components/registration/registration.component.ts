08f37ee996b190485e4fbf55f5c13612fbfcdbe5
Maciej Jamer <mjamer@concisesoftware.com>
2020-06-02 15:22:37 +0200

    add password validator

--- src/app/registration/components/registration/registration.component.ts
+++ src/app/registration/components/registration/registration.component.ts
@@ -1,3 +1,4 @@
+import { PasswordValidator } from './../../../shared/validators/password-validator';
 import { EmailDomainValidator } from './../../../shared/validators/email-domain-validator';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
@@ -17,8 +18,10 @@ export class RegistrationComponent implements OnInit {
     this.registrationForm = this.formBuilder.group({
       name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
       email: ['', [Validators.required, Validators.email, EmailDomainValidator.validateEmailDomain(emailDomain)]],
-      password: ['', [Validators.required]],
-      password2: ['', [Validators.required]],
+      passwordGroup: this.formBuilder.group({
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required]
+      }, { validator: PasswordValidator.validatePassword }),
       terms: ['', [Validators.required]]
     });
   }
