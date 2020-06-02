3436df1e22f18509b72105359ac7197503387249
Maciej Jamer <mjamer@concisesoftware.com>
2020-06-02 14:21:42 +0200

    accept email domain array instead one value

--- src/app/shared/validators/email-domain-validator.ts
+++ src/app/shared/validators/email-domain-validator.ts
@@ -1,20 +1,19 @@
 import { FormControl } from '@angular/forms';
 
 export class EmailDomainValidator {
-    static validateEmailDomain(domainName: string) {
+    static validateEmailDomain(domainName: string[]) {
         return (control: FormControl): { [key: string]: {} } | null => {
             const email = control.value;
             const domain = email.substring(email.lastIndexOf('@') + 1);
-            if (domain.toLowerCase() !== domainName.toLowerCase()) {
-                const domainError = {
+            for (const d of domainName) {
+                if (domain.toLowerCase() === d.toLowerCase()) return null;
+            }
+            return {
+                emailDomain: {
                     domain,
                     acceptDomain: domainName
-                };
-                return {
-                    emailDomain: { ...domainError }
-                };
-            }
-            return null;
+                }
+            };
         };
     }
 }
