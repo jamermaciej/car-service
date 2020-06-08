f4e3b40a15143ff71a7695fbf6fc3dfc5a8eb466
Maciej Jamer <mjamer@concisesoftware.com>
2020-06-08 20:07:13 +0200

    add app logo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ErrorMessageComponent } from './components/error-message/error-message/error-message.component';
 import { HintComponent } from './components/hint/hint/hint.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
-    declarations: [ErrorMessageComponent, HintComponent],
+    declarations: [ErrorMessageComponent, HintComponent, LogoComponent],
     imports: [
         CommonModule
     ],
     exports: [
-        CommonModule, FormsModule, ErrorMessageComponent, HintComponent
+        CommonModule, FormsModule, ErrorMessageComponent, HintComponent, LogoComponent
     ]
 })
 export class SharedModule {}
