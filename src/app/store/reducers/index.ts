73428e96dba6ea6835f93002a886d4498bfb67cd
Maciej Jamer <mjamer@concisesoftware.com>
2021-03-28 21:31:26 +0200

    clear state after logout

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,11 +1,12 @@
 import { RouterStateUrl } from './router.reducer';
 import { RouterReducerState, routerReducer } from '@ngrx/router-store';
-import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { Action, ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from 'src/environments/environment';
 import { storeFreeze } from 'ngrx-store-freeze';
 import * as fromAuth from './auth.reducer';
 import { localStorageSync } from 'ngrx-store-localstorage';
 import * as fromUI from './../../shared/store';
+import * as authActions from '../actions/auth.actions';
 
 export interface State {
     router: RouterReducerState<RouterStateUrl>;
@@ -30,8 +31,19 @@ export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionRedu
       ],
       rehydrate: true
     })(reducer);
-  }
+}
+
+export function clearState(reducer: ActionReducer<State>): ActionReducer<State> {
+  return (state: State, action: Action) => {
+
+    if (action.type === authActions.logout.type) {
+      state = undefined;
+    }
+
+    return reducer(state, action);
+  };
+}
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
-  ? [storeFreeze, localStorageSyncReducer]
+  ? [storeFreeze, localStorageSyncReducer, clearState]
   : [localStorageSyncReducer];
